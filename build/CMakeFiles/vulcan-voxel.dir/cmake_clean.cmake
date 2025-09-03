fe647277ddd3107460fb4bcd704599aebc8628d0
fabian <fabian.sucholas123@gmail.com>
2025-09-03 17:29:34 +0200

    init

---
+++ build/CMakeFiles/vulcan-voxel.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vulcan-voxel.dir/main.cpp.o"
+  "CMakeFiles/vulcan-voxel.dir/main.cpp.o.d"
+  "vulcan-voxel"
+  "vulcan-voxel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vulcan-voxel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/vulcan-voxel.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for vulcan-voxel.
+# This may be replaced when dependencies are built.
